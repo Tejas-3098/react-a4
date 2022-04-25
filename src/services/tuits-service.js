cacde595133c6c1b79403fb2c1c0766722000b5a
Tejas Sridhar <47113471+Tejas-3098@users.noreply.github.com>
2022-04-25 15:55:50 -0400

    jsdoc for tuits service

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,9 @@
+/**
+ * @file A file that offers various services to communicate with tuits related
+ * endpoints and resources.
+ */
 import axios from "axios";
 
-// const BASE_URL = "https://cs5500-01-sp22.herokuapp.com";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const TUITS_API = `${BASE_URL}/api/tuits`;
