ed9e46a7b158ec47d8a81d58446f2f5bb3ee1939
Tejas Sridhar <47113471+Tejas-3098@users.noreply.github.com>
2022-04-25 15:19:11 -0400

    jsdoc for dislikes

--- src/components/profile/my-dislikes.js
+++ src/components/profile/my-dislikes.js
@@ -1,8 +1,22 @@
+/**
+    Component of showing tuits that have been disliked by the user.
+*/
+import Tuits from "../tuits";
+import * as service from "../../services/likes-service";
+import {useEffect, useState} from "react";
+
 const MyDislikes = () => {
+    const [dislikedTuits, setDislikedTuits] = useState([]);
+    const findTuitsIDislike = () => 
+        service.findAllTuitsDislikedByUser("me")
+            .then((tuits) => setDislikedTuits(tuits));
+    useEffect(findTuitsIDislike, []);
+    
     return(
         <div>
             <h1>My Dislikes</h1>
+            <Tuits tuits={dislikedTuits} refreshTuits={findTuitsIDislike}/>
         </div>
-    )
-}
+    );
+};
 export default MyDislikes;
