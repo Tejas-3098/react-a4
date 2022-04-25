eec9fe3b01fd1ebd5878d89e11c4922c95b0613a
Tejas Sridhar <47113471+Tejas-3098@users.noreply.github.com>
2022-04-25 15:53:54 -0400

    jsdoc for likes service

--- src/services/likes-service.js
+++ src/services/likes-service.js
@@ -1,3 +1,7 @@
+/**
+ * @file A file that offers various services to communicate with likes and dislikes related
+ * endpoints and resources.
+ */
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
@@ -20,6 +24,10 @@ export const userLikesTuit = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/likes/${tid}`)
         .then(response => response.data);
 
-export const userunLikesTuit = (uid, tid) => 
-  api.put(`${USERS_API}/${uid}/unlikes/${tid}`)
+export const userDislikesTuit = (uid, tid) => 
+  api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
     .then(response => response.data);  
+
+export const findAllTuitsDislikedByUser = (userId) =>
+    api.get(`${USERS_API}/${userId}/dislikes`)
+        .then(response => response.data);
